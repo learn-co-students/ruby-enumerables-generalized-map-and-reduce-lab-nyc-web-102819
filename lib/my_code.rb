a1d065c50b830fb230c6860074bd9585fa26548c
Griffin Poole <griffinpoole5@gmail.com>
2019-10-07 18:46:53 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
 # Your Code Here
+def map(array)
+  array.length.times { |index|
+    array[index] = yield(array[index])
+  }
+  array
+end
+
+def reduce(array, start=0)
+  array.length.times { |index|
+    start = yield(array[index], start)
+    if !!start == false
+      return false
+    end
+  }
+  start
+end

225414b1f5ec06d29798d2f4ebfcca5f22ce6479
Frank Gerold <franketsgerold@gmail.com>
2019-10-14 21:48:32 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,18 @@
-# Your Code Here
+# i might be overreacting but this ACTUALLY seems mind blowing
+
+def map (array)
+  new_array =[]
+  array.length.times do
+    |entry|
+    new_array[entry] = yield(array[entry])
+  end
+  new_array
+end
+
+def reduce (array, value=0)
+  array.length.times  do
+    |entry|
+    value = yield(value, array[entry])
+  end 
+  value
+end

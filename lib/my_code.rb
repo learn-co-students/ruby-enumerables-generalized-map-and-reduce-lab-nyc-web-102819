93d0495a1ec59d68b70949c4d679a71fbd983b3b
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-14 21:11:58 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,25 @@
-def my_own_map(array)
-array.map {|n| n*-1}
+def map(array)
+  new = []
+  i = 0 
+  while i < array.length
+    new.push(yield(array[i]))
+    i += 1 
+  end
+  new 
+end
+
+def reduce(source_array, starting_value=nil)
+  if starting_value
+    sum = starting_value
+    i = 0 
+  else 
+      sum = source_array[0]
+      i = 1 
+  end 
+  
+  while i < source_array.length 
+    sum = yield(sum, source_array[i])
+    i += 1 
+  end
+  sum 
 end

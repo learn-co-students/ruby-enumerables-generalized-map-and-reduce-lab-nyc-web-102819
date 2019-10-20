e88a01e98a0f6465045397e27df532c47a3e675a
Hector Polanco <hdp2112@columbia.edu>
2019-10-20 17:38:56 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
-# Your Code Here
+def map(array)
+  new = []
+  array.length.times do |index|
+    new << yield(array[index])
+  end
+  new
+end
+
+def reduce(array, starting_point=nil)
+  if starting_point
+    total = starting_point
+    index = 0
+  else
+    total = array[0]
+    index = 1
+  end
+  while index < array.length
+    total = yield(total, array[index])
+    index += 1
+  end
+  total
+end

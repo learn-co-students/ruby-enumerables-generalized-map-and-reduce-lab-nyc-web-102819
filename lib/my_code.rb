c32f25a56317005637d9b88588a7ca32e9f79487
Hasibul Chowdhury <hasibulc91@gmail.com>
2019-10-25 21:42:13 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,53 @@
-# Your Code Here
+def map_to_negativize(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * -1 ) # <== Unique work
+    i += 1
+  end
+  return new
+end
+ 
+def map_to_no_change(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] ) # <== Unique work
+    i += 1
+  end
+  return new
+end
+ 
+def map_to_double(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * 2 ) # <== Unique work
+    i += 1
+  end
+  return new
+end
+ 
+def map_to_square(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * source_array[i] ) # <== Unique work
+    i += 1
+  end
+  return new
+end
 
-# Map
 
-#negative
 
-[].map{|n| n * -1}
 
+
+
+
+map_to_negativize(source_array){|n| n * -1}
+
+map_to_no_change(source_array){|n| n}
+
+map_to_double(source_array){|n| n * 2}
+
+map_to_square(source_array){|n| n * n}
